06734bdb8b56632832876fcaceefb12d51c95ebf
Hazzu <hailaconsutu558@gmail.com>
2024-07-20 22:42:37 +0700

    [REFACTOR] Change position middleware isAuthenticate

--- server/app.js
+++ server/app.js
@@ -21,13 +21,15 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 // Register router - begin
+const { isAuthenticated } = require("./src/auth/middleware");
+
 const authRouter = require("./src/auth/route");
 const userRouter = require("./src/user/route");
 const roleRouter = require("./src/role/route");
 
 app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/users", userRouter);
-app.use("/api/v1/roles", roleRouter);
+app.use("/api/v1/users", isAuthenticated, userRouter);
+app.use("/api/v1/roles", isAuthenticated, roleRouter);
 // Register router - end
 
 module.exports = app;
--- server/src/user/route.js
+++ server/src/user/route.js
@@ -1,16 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const { index, create, destroy, update } = require("./service");
-const { isAuthenticated } = require("../auth/middleware");
 const {
     validateQueryUserIndex,
     validateRegister,
     validateChangePassword,
 } = require("./validate");
 
-router.get("/", isAuthenticated, validateQueryUserIndex, index);
-router.post("/", isAuthenticated, validateRegister, create);
-router.patch("/:username", isAuthenticated, validateChangePassword, update);
-router.delete("/:username", isAuthenticated, destroy);
+router.get("/", validateQueryUserIndex, index);
+router.post("/", validateRegister, create);
+router.patch("/:username", validateChangePassword, update);
+router.delete("/:username", destroy);
 
 module.exports = router;
