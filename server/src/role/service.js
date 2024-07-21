c5a4646fce7444e62351abca6872ae7661ab6aac
Hazzu <hailaconsutu558@gmail.com>
2024-07-21 16:04:04 +0700

    [ADD] Add feature create and update role

--- server/src/role/service.js
+++ server/src/role/service.js
@@ -5,10 +5,8 @@ const HTTP_STATUS_CODE = require("../../utils/httpStatusCode");
 /**
  * API trả về danh sách role
  *
- * URI: /api/v1/roles
- *
- * Method: GET
- *
+ * @path /api/v1/roles
+ * @method GET
  * @param {express.Request} req
  * @param {express.Response} res
  */
@@ -33,6 +31,84 @@ const index = async (req, res) => {
     }
 };
 
+/**
+ * API thêm role mới và hệ thống
+ *
+ * @path /api/v1/roles
+ * @method POST
+ * @param {express.Request} req
+ * @param {express.Response} res
+ */
+const create = async (req, res) => {
+    try {
+        // Thêm role vào database
+        await db.sequelize.transaction(async (t) => {
+            const roleName = req.body.name;
+
+            await db.Role.create({
+                name: roleName,
+            });
+        });
+
+        return res.status(HTTP_STATUS_CODE.CREATED).send({
+            status: "success",
+            message: "Thêm role thành công",
+        });
+    } catch (error) {
+        console.log(error);
+
+        return res.status(HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR).send({
+            status: "error",
+            message: "Lỗi máy chủ",
+        });
+    }
+};
+
+/**
+ * API cập nhật thông tin của role
+ *
+ * @path /api/v1/roles/:roleId
+ * @method PATCH
+ * @param {express.Request} req
+ * @param {express.Response} res
+ */
+const update = async (req, res) => {
+    try {
+        // Lấy role cần chỉnh sửa
+        const roleId = req.params.roleId;
+        const role = await db.Role.findOne({ where: { id: roleId } });
+
+        if (!role) {
+            return res.status(HTTP_STATUS_CODE.NOT_FOUND).send({
+                status: "error",
+                message: "Không tìm thấy tài nguyên",
+            });
+        }
+
+        // Chỉnh sửa thông tin role
+        await db.sequelize.transaction(async (t) => {
+            const roleName = req.body.name;
+
+            role.name = roleName;
+            await role.save();
+        });
+
+        return res.status(HTTP_STATUS_CODE.CREATED).send({
+            status: "success",
+            message: "Chỉnh sửa thông tin thành công",
+        });
+    } catch (error) {
+        console.log(error);
+
+        return res.status(HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR).send({
+            status: "error",
+            message: "Lỗi máy chủ",
+        });
+    }
+};
+
 module.exports = {
     index,
+    create,
+    update,
 };
