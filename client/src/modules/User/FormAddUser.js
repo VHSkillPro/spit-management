9e080ed0295de14664ced464968a3aff96da388f
Hazzu <hailaconsutu558@gmail.com>
2024-07-24 00:22:54 +0700

    [UPDATE] Update form add user and add validate

--- client/src/modules/User/FormAddUser.js
+++ client/src/modules/User/FormAddUser.js
@@ -5,6 +5,7 @@ import {
     CardBody,
     CardHeader,
     CardTitle,
+    Form,
     FormControl,
     FormGroup,
     FormLabel,
@@ -16,16 +17,18 @@ import { useMessage } from "../../contexts/MessageContext";
 import { ToastStatus } from "../../components/Toast";
 import { getAllRoles } from "../../API/roleService";
 import { createUser } from "../../API/userService";
+import { useForm } from "react-hook-form";
 
 export default function FormAddUser() {
     const navigate = useNavigate();
     const [roles, setRoles] = useState();
     const { handleAddMessage } = useMessage();
 
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [repassword, setRepassword] = useState("");
-    const [roleId, setRoleId] = useState(0);
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
 
     useEffect(() => {
         getAllRoles()
@@ -46,9 +49,9 @@ export default function FormAddUser() {
     }, []);
 
     // Hàm xử lý khi nhấn nút thêm
-    const handleAddUser = () => {
+    const handleAddUser = (data) => {
         // Kiểm tra xem mật khẩu và nhập lại mật khẩu có khớp nhau không
-        if (password !== repassword) {
+        if (data.password !== data.repassword) {
             handleAddMessage(
                 "Thất bại",
                 "Mật khẩu không khớp",
@@ -60,27 +63,18 @@ export default function FormAddUser() {
         // Tạo object user
         const user = {};
 
-        // Nếu username không rỗng thì gán vào user
-        if (username) {
-            user.username = username;
-        }
-
-        // Nếu password không rỗng thì gán vào user
-        if (password) {
-            user.password = password;
-        }
-
-        // Nếu repassword không rỗng thì gán vào user
-        if (repassword) {
-            user.repassword = repassword;
+        // Gắn dữ liệu của các trường có kiểu string
+        const stringField = ["username", "password", "repassword"];
+        for (let field of stringField) {
+            if (data[field]) {
+                user[field] = data[field];
+            }
         }
 
         // Nếu roleId khác 0 thì gán vào user
-        if (roleId) {
-            user.roleId = parseInt(roleId);
+        if (parseInt(data.roleId)) {
+            user.roleId = parseInt(data.roleId);
         }
-
-        console.log(user);
 
         // Gọi API thêm tài khoản
         createUser(user)
@@ -94,11 +88,9 @@ export default function FormAddUser() {
             })
             .catch((error) => {
                 let message = "Thông tin không hợp lệ, vui lòng kiểm tra lại";
-
                 if (error.response.status === 409) {
                     message = "Tên tài khoản đã tồn tại";
                 }
-
                 handleAddMessage("Thất bại", message, ToastStatus.DANGER);
             });
     };
@@ -109,78 +101,143 @@ export default function FormAddUser() {
                 <CardTitle className="fw-bold fs-5">Thêm tài khoản</CardTitle>
             </CardHeader>
             <CardBody>
-                <FormGroup className="mb-3">
-                    <FormLabel className="fs-6">Tên tài khoản</FormLabel>
-                    <FormControl
-                        value={username}
-                        placeholder="Nhập tên tài khoản"
-                        onChange={(e) => setUsername(e.target.value)}
-                    ></FormControl>
-                    <ul>
-                        <li>
-                            <FormText>
-                                Tên tài khoản phải có độ dài từ 5 đến 255 ký tự.
-                            </FormText>
-                        </li>
-                    </ul>
-                </FormGroup>
-                <FormGroup className="mb-3">
-                    <FormLabel className="fs-6">Mật khẩu</FormLabel>
-                    <FormControl
-                        type="password"
-                        value={password}
-                        placeholder="Nhập mật khẩu"
-                        onChange={(e) => setPassword(e.target.value)}
-                    ></FormControl>
-                    <ul>
-                        <li>
-                            <FormText>
-                                Mật khẩu chỉ chứa các ký tự in thường (a-z), in
-                                hoa (A-Z), ký tự số (0-9) và ký tự đặc biệt
-                                (@.#$!%*?&^).
-                            </FormText>
-                        </li>
-                        <li>
-                            <FormText>
-                                Mật khẩu phải có độ dài từ 6 đến 255 ký tự.
-                            </FormText>
-                        </li>
-                    </ul>
-                </FormGroup>
-                <FormGroup className="mb-3">
-                    <FormLabel className="fs-6">Nhập lại mật khẩu</FormLabel>
-                    <FormControl
-                        type="password"
-                        value={repassword}
-                        placeholder="Nhập lại mật khẩu"
-                        onChange={(e) => setRepassword(e.target.value)}
-                    ></FormControl>
-                </FormGroup>
-                {roles && (
+                <Form onSubmit={handleSubmit(handleAddUser)}>
+                    <FormGroup className="mb-3">
+                        <FormLabel className="fs-6">Tên tài khoản</FormLabel>
+                        <FormControl
+                            placeholder="Nhập tên tài khoản"
+                            {...register("username", {
+                                required: true,
+                                minLength: 5,
+                                maxLength: 255,
+                            })}
+                        ></FormControl>
+                        <ul>
+                            {errors.username?.type === "required" && (
+                                <li>
+                                    <FormText className="text-danger">
+                                        Tên tài khoản không được để trống
+                                    </FormText>
+                                </li>
+                            )}
+                            <li>
+                                <FormText
+                                    className={
+                                        errors.username?.type === "minLength" ||
+                                        errors.username?.type === "maxLength"
+                                            ? "text-danger"
+                                            : ""
+                                    }
+                                >
+                                    Tên tài khoản phải có độ dài từ 5 đến 255 ký
+                                    tự.
+                                </FormText>
+                            </li>
+                        </ul>
+                    </FormGroup>
+                    <FormGroup className="mb-3">
+                        <FormLabel className="fs-6">Mật khẩu</FormLabel>
+                        <FormControl
+                            type="password"
+                            placeholder="Nhập mật khẩu"
+                            {...register("password", {
+                                required: true,
+                                minLength: 5,
+                                maxLength: 255,
+                                pattern: /^[a-zA-Z0-9@.#$!%*?&^]+$/,
+                            })}
+                        ></FormControl>
+                        <ul>
+                            {errors.password?.type === "required" && (
+                                <li>
+                                    <FormText className="text-danger">
+                                        Mật khẩu không được để trống
+                                    </FormText>
+                                </li>
+                            )}
+                            <li>
+                                <FormText
+                                    className={
+                                        errors.password?.type === "minLength" ||
+                                        errors.password?.type === "maxLength"
+                                            ? "text-danger"
+                                            : ""
+                                    }
+                                >
+                                    Mật khẩu phải có độ dài từ 6 đến 255 ký tự.
+                                </FormText>
+                            </li>
+                            <li>
+                                <FormText
+                                    className={
+                                        errors.password?.type === "pattern"
+                                            ? "text-danger"
+                                            : ""
+                                    }
+                                >
+                                    Mật khẩu chỉ chứa các ký tự in thường (a-z),
+                                    in hoa (A-Z), ký tự số (0-9) và ký tự đặc
+                                    biệt (@.#$!%*?&^).
+                                </FormText>
+                            </li>
+                        </ul>
+                    </FormGroup>
                     <FormGroup className="mb-3">
                         <FormLabel className="fs-6">
-                            Chức vụ trong hệ thống
+                            Nhập lại mật khẩu
                         </FormLabel>
-                        <FormSelect
-                            name="role"
-                            value={roleId}
-                            onChange={(e) => setRoleId(e.target.value)}
-                        >
-                            <option value={0}>Chọn chức vụ</option>
-                            {roles.map((role, idx) => (
-                                <option key={idx} value={role.id}>
-                                    {role.name}
-                                </option>
-                            ))}
-                        </FormSelect>
+                        <FormControl
+                            type="password"
+                            placeholder="Nhập lại mật khẩu"
+                            {...register("repassword", {
+                                required: true,
+                            })}
+                        ></FormControl>
+                        {errors.repassword?.type === "required" && (
+                            <ul>
+                                <li>
+                                    <FormText className="text-danger">
+                                        Mật khẩu không được để trống
+                                    </FormText>
+                                </li>
+                            </ul>
+                        )}
                     </FormGroup>
-                )}
-                <FormGroup className="d-flex justify-content-center">
-                    <Button onClick={handleAddUser}>
-                        <i className="bi bi-plus-circle me-2"></i>
-                        Thêm
-                    </Button>
-                </FormGroup>
+                    {roles && (
+                        <FormGroup className="mb-3">
+                            <FormLabel className="fs-6">
+                                Chức vụ trong hệ thống
+                            </FormLabel>
+                            <FormSelect
+                                {...register("roleId", {
+                                    validate: (value) => value > 0,
+                                })}
+                            >
+                                <option value={0}>Chọn chức vụ</option>
+                                {roles.map((role, idx) => (
+                                    <option key={idx} value={role.id}>
+                                        {role.name}
+                                    </option>
+                                ))}
+                            </FormSelect>
+                            {errors.roleId?.type === "validate" && (
+                                <ul>
+                                    <li>
+                                        <FormText className="text-danger">
+                                            Vui lòng chọn chức vụ
+                                        </FormText>
+                                    </li>
+                                </ul>
+                            )}
+                        </FormGroup>
+                    )}
+                    <FormGroup className="d-flex justify-content-center">
+                        <Button type="submit">
+                            <i className="bi bi-plus-circle me-2"></i>
+                            Thêm
+                        </Button>
+                    </FormGroup>
+                </Form>
             </CardBody>
         </Card>
     );
